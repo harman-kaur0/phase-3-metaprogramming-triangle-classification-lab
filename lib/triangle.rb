e149c5b75f341b3d6746b50573a00b87dea8ad94
Harmandeep Kaur <harmandeepk628@gmail.com>
2021-05-23 16:19:34 -0700

    commit tree f5add8ce93ee868e4c019e626e350ae4e386b008

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_accessor :side_1, :side_2, :side_3
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+    is_valid?
+
+  end
+
+  def kind
+    if @side_1 == @side_2 && @side_1 == @side_3
+      :equilateral
+    elsif @side_1 != @side_2 && @side_1 != @side_3 && @side_2 != @side_3
+      :scalene
+    else
+      :isosceles
+    end
+  end
+
+  def is_valid?
+    if @side_1 > 0 && @side_2 > 0 && @side_3 > 0 && @side_1 + @side_2 > @side_3 && @side_2 + @side_3 > @side_1 && @side_1 + @side_3 > @side_2
+      nil
+    else 
+      raise TriangleError
+    end 
+  end
+    
+  class TriangleError < StandardError
+  end
 end
